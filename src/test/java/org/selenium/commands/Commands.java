65270e8694bd06d23fd584d12489043c840399e9
Shifana-123 <sshifana959@gmail.com>
2023-11-27 20:58:17 +0530

    DemoTour

--- src/test/java/org/selenium/commands/Commands.java
+++ src/test/java/org/selenium/commands/Commands.java
@@ -21,20 +21,52 @@ public class Commands extends Base {
         Assert.assertEquals(actualSwagLogoText, expectedSwagLogoText, "Invalid Logo found in login page");
 
     }
+
     @Test
-    public void verifyDemoWebShopUserLogin()
-    {
+    public void verifyDemoWebShopUserLogin() {
         driver.get("https://demowebshop.tricentis.com/");
-        WebElement login= driver.findElement(By.className("ico-login"));
+        WebElement login = driver.findElement(By.className("ico-login"));
         login.click();
-        WebElement emailField=driver.findElement(By.id("Email"));
+        WebElement emailField = driver.findElement(By.id("Email"));
         emailField.sendKeys("sshifana959@gmail.com");
-        WebElement passwordField=driver.findElement(By.id("Password"));
+        WebElement passwordField = driver.findElement(By.id("Password"));
         passwordField.sendKeys("Shifana@123");
-        WebElement loginButton=driver.findElement(By.className("login-button"));
+        WebElement loginButton = driver.findElement(By.className("login-button"));
         loginButton.click();
-        String actualUrl=driver.getCurrentUrl();
-        String expectedUrl="https://demowebshop.tricentis.com/";
-        Assert.assertEquals(actualUrl,expectedUrl,"Invalid URL found");
+        WebElement UserLogin = driver.findElement(By.className("account"));
+        String actualUserLogin = UserLogin.getText();
+        String expectedUserLogin = "sshifana959@gmail.com";
+
+
+        Assert.assertEquals(actualUserLogin, expectedUserLogin, "Login Failed");
+    }
+
+    @Test
+    public void verifyDemoTourRegistration() {
+        driver.get("https://demo.guru99.com/test/newtours/register.php");
+        WebElement firstNameField = driver.findElement(By.name("firstName"));
+        firstNameField.sendKeys("Shifana");
+        WebElement lastNameField = driver.findElement(By.name("lastName"));
+        lastNameField.sendKeys("S");
+        WebElement phoneNumberField = driver.findElement(By.name("phone"));
+        phoneNumberField.sendKeys("8156963192");
+        WebElement emailField = driver.findElement(By.id("userName"));
+        emailField.sendKeys("sshifana959@gmail.com");
+        WebElement addressField = driver.findElement(By.name("address1"));
+        addressField.sendKeys("Attingal");
+        WebElement cityName = driver.findElement(By.name("city"));
+        cityName.sendKeys("Thiruvananthapuram");
+        WebElement stateName = driver.findElement(By.name("state"));
+        stateName.sendKeys("Kerala");
+        WebElement postalCodeField = driver.findElement(By.name("postalCode"));
+        postalCodeField.sendKeys("695305");
+        WebElement userNameField = driver.findElement(By.id("email"));
+        userNameField.sendKeys("sshifana959@gmail.com");
+        WebElement passWordField = driver.findElement(By.name("password"));
+        passWordField.sendKeys("Demo@123");
+        WebElement confirmPassWordField = driver.findElement(By.name("confirmPassword"));
+        confirmPassWordField.sendKeys("Demo@123");
+        WebElement submitButton = driver.findElement(By.name("submit"));
+        submitButton.click();
     }
 }
