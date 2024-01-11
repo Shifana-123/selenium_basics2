1b4ba7e6dbd89d015899b24b332d5b28437f9d3b
Shifana-123 <sshifana959@gmail.com>
2024-01-11 22:29:12 +0530

    new commit

--- src/test/java/org/selenium/page/RegisterPage.java
+++ src/test/java/org/selenium/page/RegisterPage.java
@@ -1,10 +1,59 @@
 package org.selenium.page;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.selenium.utilities.WaitUtility;
+import org.selenium.utilities.WebElementUtility;
 
 public class RegisterPage {
     WebDriver driver;
     public RegisterPage(WebDriver driver) {
         this.driver=driver;
+        PageFactory.initElements(driver,this);
     }
+   @FindBy(id="gender-female")
+    WebElement genderFemale;
+
+    @FindBy(id="FirstName")
+    WebElement firstNameField;
+
+    @FindBy(id="LastName")
+    WebElement lastNameField;
+
+    @FindBy(id="Email")
+    WebElement emailField;
+
+    @FindBy(id="Password")
+    WebElement passwordField;
+
+    @FindBy(id = "ConfirmPassword")
+    WebElement confirmPasswordField;
+
+    @FindBy(id = "register-button")
+    WebElement registerButton;
+
+    public void clickOnElement() {
+        WebElementUtility.clickOnElement(genderFemale);
+    }
+    public void enterFirstName(String firstname) {
+        WebElementUtility.enterValue(firstNameField,firstname );
+    }
+    public void enterLastName(String lastname) {
+        WebElementUtility.enterValue(lastNameField,lastname);
+    }
+    public void enterEmail(String email) {
+        WebElementUtility.enterValue(emailField,email);
+    }
+    public void enterPassword(String password) {
+        WebElementUtility.enterValue(passwordField,password);
+    }
+    public void enterConfirmPassword(String password) {
+        WebElementUtility.enterValue(confirmPasswordField,password);
+    }
+    public void clickOnRegisterButton() {
+        WebElementUtility.clickOnElement(registerButton);
+    }
+
 }
--- src/test/java/org/selenium/test/RegisterPageTest.java
+++ src/test/java/org/selenium/test/RegisterPageTest.java
@@ -1,6 +1,24 @@
 package org.selenium.test;
 
 import org.selenium.automationcore.Base;
+import org.selenium.page.HomePage;
+import org.selenium.page.RegisterPage;
+import org.selenium.utilities.RandomDataUtility;
 
 public class RegisterPageTest extends Base {
+public void verifyRegistrationWithValidCredentials()  {
+    String firstName = RandomDataUtility.getFirstName();
+    String lastName = RandomDataUtility.getLastName();
+    String emailID = firstName + "." + lastName + "123@yahoo.com";
+    String password = firstName + "." + lastName;
+    HomePage home=new HomePage(driver);
+    RegisterPage register= home.clickOnRegisterMenu();
+    register.clickOnElement();
+    register.enterFirstName(firstName);
+    register.enterLastName(lastName);
+    register.enterEmail(emailID);
+    register.enterPassword(password);
+    register.enterConfirmPassword(password);
+    register.clickOnRegisterButton();
+}
 }
